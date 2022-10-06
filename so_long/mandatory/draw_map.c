6bbd9cf8bd6638cec11baa42abad1f2142845c96
mini <ske07137@naver.com>
2022-10-07 02:36:05 +0900

    bab

--- so_long/mandatory/draw_map.c
+++ so_long/mandatory/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: seojo <seojo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 23:21:15 by seojo             #+#    #+#             */
-/*   Updated: 2022/10/07 02:01:56 by seojo            ###   ########.fr       */
+/*   Updated: 2022/10/07 02:33:42 by seojo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,37 @@ int	open_map(char *map)
 	return (fd);
 }
 
-
-int	map_obj_check(char *map, int *objs)
+int	map_obj_check(char c, int *objs)
 {
-	int	i;
-
-	i = 1;
-	while (map[++i])
-	{
-		if (map[i] == '1' || map[i] == '0')
-			continue ;
-		else if (map[i] == 'C')
-			objs[COIN]++;
-		else if (map[i] == 'E')
-			objs[EXIT]++;
-		else if (map[i] == 'P')
-			objs[P]++;
-		else
-			return (1);
-	}
+	if (c == '1' || c == '0')
+		continue ;
+	else if (c == 'C')
+		objs[COIN]++;
+	else if (c == 'E')
+		objs[EXIT]++;
+	else if (c == 'P')
+		objs[P]++;
+	else
+		return (1);
 	return (0);
 }
 
-void	map_malloc(t_game *game, int fd)
+void	map_size_objs_ck(t_game *game, int fd)
 {
 	int		i;
 	char	c;
+	int		objs[3];
 
+	objs[P] = 0;
+	objs[COIN] = 0;
+	objs[EXIT] = 0; 
 	i = 0;
 	while (read(fd, &c, 1) > 0)
 	{
-		game->map_cols = i * (i > game->map_cols);
+		if (map_obj_check(c, objs))
+			ft_error("objs error");
+		if (game->map_cols < i)
+			game->map_cols = i;
 		if (c == '\n')
 		{
 			game->map_rows++;
@@ -61,50 +61,76 @@ void	map_malloc(t_game *game, int fd)
 		else
 			i++;
 	}
+	if (objs[P] != 1 || objs[COIN] < 1 || objs[EXIT] < 1)
+		ft_error("object count error");
+}
+
+void	map_malloc(t_game *game, int fd)
+{
+	int	i;
+
+	map_size_objs_ck(game, fd);
 	game->map_arr = (char **)malloc(sizeof(char *) * game->map_rows);
-	if (!game->map_arr || game->map_cols == 0)
+	if (!game->map_arr)
 		ft_error("malloc error");
 	i = -1;
 	while (++i != game->map_cols)
 	{
 		game->map_arr[i] = (char *)malloc(sizeof(char *) * game->map_cols);
 		if (!game->map_arr[i])
-			ft_error("malloc error");	
+			ft_error("malloc error");
 	}
 }
 
 void	map_load(char *map, t_game *game, int fd)
 {
-	int		fd;
 	char	*line;
-	int		objs[3];
-	int i;
-	int j;
+	int		fd;
+	int		i;
+	int		j;
 
-	objs[P] = 0;
-	objs[COIN] = 0;
-	objs[EXIT] = 0;  //bit mask
 	fd = open_map(map);
 	line = get_next_line(fd);
-	game->map_wid = ft_strlen(line); // -1;
+	game->map_wid = ft_strlen(line);        // -1;
 	i = 0;
 	while (line)
 	{
 		game->map_hei++;
-		if (map_obj_check(line, objs))
-			ft_error("obj error");
-		if (game_wid != ft_strlen(line))  // -1;
+		if (game_wid != ft_strlen(line))    // -1;
 			ft_error("map width error");
+		j = 0;
+		while (j < game->map_cols)
+		{
+			game->map_arr[i][j] = line[j];
+			j++;
+		}
 		free(line);
 		line = get_next_line(fd);
 	}
 	close(fd);
-	if (objs[P] != 1 || objs[COIN] < 1 || objs[EXIT] < 1)
-		ft_error("object count error");
 }
-//배열에 담아서 그리기
 
-//주변 벽 이 돌아가있는지 네모 체크
+void	map_wall_ck(t_game *game)
+{
+	int	i;
+	
+	i = 0;
+	while (game->map_arr[0][i])
+		if (game->map->arr[0][i++] != '1')
+			ft_error("wall error");
+	i = 0;
+	while (game->map_arr[i][0])
+		if (game->map->arr[i++][0] != '1')
+			ft_error("wall error");
+	i = 0;
+	while (game->map_arr[i][game->map_cols - 1])
+		if (game->map->arr[i++][game->map_cols - 1] != '1')
+			ft_error("wall error");
+	i = 0;
+	while (game->map_arr[game->map_rows - 1][i])
+		if (game->map->arr[game->map_rows - 1][i++] != '1')
+			ft_error("wall error");
+}
 
 void	map_init(char *map, t_game *game)
 {
@@ -114,7 +140,8 @@ void	map_init(char *map, t_game *game)
 	if (map_name_len < 5 || ft_strncmp(map[map_name_len - 4], ".ber" ,4))
 		ft_error(0);
 	fd = open_map(map);
-	map_malloc(game);
+	map_malloc(game, fd);
 	close(fd);
-	map_load(map, game, fd);
+	map_load(map, game);
+	map_wall_ck(game);
 }
