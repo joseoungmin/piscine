a4ef8ebacdd4368f9011bc50068c1a0a30e66b8b
Seongmin Jo <seojo@cx2r2s6.42seoul.kr>
2023-01-10 19:27:51 +0900

    err ing

--- cub3D/main.c
+++ cub3D/main.c
@@ -6,14 +6,33 @@
 /*   By: seojo <seojo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 19:12:22 by seojo             #+#    #+#             */
-/*   Updated: 2023/01/10 19:13:19 by seojo            ###   ########.fr       */
+/*   Updated: 2023/01/10 19:27:34 by seojo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int main(void)
-{
+typedef enum e_error t_err;
 
+enum e_error
+{
+	ARGU,
+};
+
+void	error_exit(t_err err_no)
+{
+	const char	e_arr[][] = {
+		"Error
+}
+
+void	check_argument(int ac, char **av)
+{
+	if (ac != 2)
+		error_exit(
+}
+
+int	main(int ac, char **av)
+{
+	check_argument(ac, av);
 	return (0);
 }
