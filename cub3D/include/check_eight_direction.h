bde52b6a91abaa1ffd1bfaa14c4b81ddad5a88e7
mini <ske07137@naver.com>
2023-01-15 16:45:56 +0900

    header norm

--- cub3D/include/check_eight_direction.h
+++ cub3D/include/check_eight_direction.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_eight_direction.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seojo <seojo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 16:43:50 by seojo             #+#    #+#             */
+/*   Updated: 2023/01/15 16:44:35 by seojo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CHECK_EIGHT_DIRECTION_H
 # define CHECK_EIGHT_DIRECTION_H
 # include "map.h"
 # include "error.h"
 
-void    check_eight_directions(char **map, int height, int width);
+void	check_eight_directions(char **map, int height, int width);
 
 #endif
