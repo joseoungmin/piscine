a754a17c4400550ec86c1c1db3167495d0d66d4b
mini <ske07137@naver.com>
2023-01-15 16:47:56 +0900

    main.h del include files

--- cub3D/include/main.h
+++ cub3D/include/main.h
@@ -6,17 +6,13 @@
 /*   By: seojo <seojo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:01:48 by seojo             #+#    #+#             */
-/*   Updated: 2023/01/15 15:01:40 by seojo            ###   ########seoul.kr  */
+/*   Updated: 2023/01/15 16:47:12 by seojo            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAIN_H
 # define MAIN_H
 # include <stdio.h>
-# include <sys/errno.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <fcntl.h>
 # include <math.h>
 # include "mlx.h"
 # include "check_argument.h"
