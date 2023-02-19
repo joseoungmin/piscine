73331a3f8626fd381e663f1a00fe005fcf004dd2
Seongmin Jo <seojo@c3r5s2.42seoul.kr>
2023-02-19 19:56:59 +0900

    color fixing

--- cub3D/src/main.c
+++ cub3D/src/main.c
@@ -6,11 +6,40 @@
 /*   By: seojo <seojo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 19:12:22 by seojo             #+#    #+#             */
-/*   Updated: 2023/02/19 13:44:15 by seojo            ###   ########.fr       */
+/*   Updated: 2023/02/19 19:55:53 by seojo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+
+#include <stdio.h>
+void	print_map(t_world *world)
+{
+	int	y;
+	int	x;
+
+	printf("\n========== PRINT OBJ ==========\n\n");
+	printf("NO = [%s]\n", world->tex_path[NO]);
+	printf("SO = [%s]\n", world->tex_path[SO]);
+	printf("WE = [%s]\n", world->tex_path[WE]);
+	printf("EA = [%s]\n", world->tex_path[EA]);
+	printf("F = [%x]\n", world->floor);
+	printf("C = [%x]\n", world->ceiling);
+	printf("player direction = %c\n", world->player.cardinal);
+	printf("player x = %f\n", world->player.pos.x);
+	printf("player y = %f\n", world->player.pos.y);
+	printf("\n========== PRINT MAP ==========\n\n");
+	y = 0;
+	while (y < world->map_h)
+	{
+		x = 0;
+		while (x < world->map_w)
+			printf("%c", world->map[y][x++]);
+		y++;
+		printf("\n");
+	}
+	printf("\n ========== END MAP ========== \n");
+}
 
 t_world	*init_world(void)
 {
@@ -51,6 +80,7 @@ int	main(int ac, char **av)
 	world = init_world();
 	init_player(&world->player);
 	check_map(world, av[1]);
+	print_map(world);
 	init_render(world);
 	render(world);
 	clear_world(world);
--- cub3D/src/map_parse/check_map_color.c
+++ cub3D/src/map_parse/check_map_color.c
@@ -6,7 +6,7 @@
 /*   By: seojo <seojo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 12:32:41 by seojo             #+#    #+#             */
-/*   Updated: 2023/02/19 19:30:22 by seojo            ###   ########.fr       */
+/*   Updated: 2023/02/19 19:48:59 by seojo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*split_color(char *read_line, int *color)
 {
 	int	i;
 
+	if (!ft_isdigit(*read_line))
+		err_exit("invalid color object");
 	read_line = pass_space(read_line);
 	*color = ft_atoi(read_line);
 	i = 1;
