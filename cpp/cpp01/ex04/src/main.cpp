7f23cc40d6b1ed1e8b4415ceccb4756137d2e0c2
mini <ske07137@naver.com>
2023-03-22 13:36:14 +0900

    cpp01/ex04

---
+++ cpp/cpp01/ex04/src/main.cpp
@@ -0,0 +1,39 @@
+#include "replace.hpp"
+
+int main(int ac, char **av){
+	std::string 	s1;
+	std::string 	s2;
+	std::string 	line;
+	std::string		newLine;
+	std::ifstream	ifs;
+	std::ofstream	ofs;
+
+	if (ac != 4){
+		std::cout << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
+		return (0);
+	}
+	ifs.open(av[1]);
+	if (!ifs.is_open()){
+		std::cout << "Error: file not found" << std::endl;
+		return (0);
+	}
+	s1 = av[2];
+	s2 = av[3];
+	ofs.open(av[1] + std::string(".replace"));
+	if (!ofs.is_open()){
+		std::cout << "Error: failed to open output file" << std::endl;
+		return (0);
+	}
+	while (!ifs.eof() && std::getline(ifs, line)){
+		while (line.find(s1) != std::string::npos){
+			newLine = line.substr(0, line.find(s1));
+			newLine += s2;
+			newLine += line.substr(line.find(s1) + s1.length());
+			line = newLine;
+		}
+		ofs << line << std::endl;
+	}
+	ifs.close();
+	ofs.close();
+	return (0);
+}
