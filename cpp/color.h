4c5b9fab830246081c921fa556121dfdf9cdec34
mini <ske07137@naver.com>
2023-04-16 21:26:29 +0900

    update

---
+++ cpp/color.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_HPP
+#define COLOR_HPP
+
+/*****  Standard Color  *****/
+# define C_RESET        "\033[0m"
+# define C_RED          "\033[31m"
+# define C_GREEN        "\033[32m"
+# define C_YELLOW       "\033[33m"
+# define C_BLUE         "\033[34m"
+# define C_MAGENTA      "\033[35m"
+# define C_CYAN         "\033[36m"
+# define C_WHITE        "\033[37m"
+
+/*****  Bright Color    *****/
+# define BC_RED         "\033[91m"
+# define BC_GREEN       "\033[92m"
+# define BC_YELLOW      "\033[93m"
+# define BC_BLUE        "\033[94m"
+# define BC_MAGENTA     "\033[95m"
+# define BC_CYAN        "\033[96m"
+# define BC_WHITE       "\033[97m"
+
+#endif
--- cpp/cpp02/ex03/src/bsp.cpp
+++ cpp/cpp02/ex03/src/bsp.cpp
@@ -1,5 +1,8 @@
 #include "Point.hpp"
 
+// outerProduct가 외적을 구하는 공식 
+// 각 변마다 외적을 구해서 zero가 하나라도 있으면 변에 맞다은 부분이라 외부로 처리3개 값이 모두 음수거나 모두 양수로 동일하면 내부로 처리 
+
 Fixed	outerProduct(Point const p1, Point const p2, Point const p3) {
 	return ((p1.getX() - p3.getX()) * (p2.getY() - p3.getY())
 		- (p2.getX() - p3.getX()) * (p1.getY() - p3.getY()));
